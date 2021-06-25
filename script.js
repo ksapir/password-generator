020a03e80fa4b8da99c310431dbdf2658233b1c8
Karen Pion <karenlpion@gmail.com>
2021-06-25 02:13:47 -0400

    adding randomly generated results from arrays

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 // // Assignment Code
 var pwLength = [];
-
-var invalid = isNaN(pwLength)
-
+var invalid = [isNaN(pwLength), pwLength>= 8, pwLength<= 128];
 var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K'", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"
 ];
-
 var lowerCase= "abcdefghijklmnopqurstuvwxyz".split("");
-
 var pwNum= [1,2,3,4,5,6,7,8,9,0];
-
 var special= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{","}", "|", ";",";", "'", "<", ">", "?", "/", ",", ".", "`", "~"];
-
 var generateBtn = document.querySelector("#generate");
 
 // function generatePassword(){
@@ -19,7 +13,7 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.onclick = () => {
     var pwLength = prompt ("How long would like your password to be? (8-128)");
     console.log(pwLength);
-userPwLength = function (){
+
    if (pwLength>= 8 && pwLength<= 128){
         var lowerCase = confirm ("Would you like to include lowercase letters?");
             console.log(lowerCase);
@@ -29,19 +23,32 @@ userPwLength = function (){
             console.log(pwNum);
         var special = confirm ("Would you like to include special characters?");
             console.log(special);
-
 }  else if (invalid){
-    alert("Invalid response");
-    var pwLength = prompt("How long would like your password to be? (8-128)");
-    console.log(pwLength);
+    alert("Invalid response. Click 'Generate Password Button' again");
+    
 }   else {
-        alert("Invalid response");
-        var pwLength = prompt("How long would like your password to be? (8-128)");
-        console.log(pwLength);
-}
-while (invalid=true && pwLength>=8 && pwLength<= 128)
+        alert("Invalid response. Click 'Generate Password Button' again");
 }
 }
+
+   var getLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        console.log(getLowerCase);
+
+    var getUpperCase = upperCase[Math.floor(Math.random() * upperCase.length)];
+        console.log(getUpperCase);
+
+    var randomNum = pwNum[Math.floor(Math.random() * pwNum.length)];
+        console.log(randomNum);
+ 
+    var randomSpecial = special[Math.floor(Math.random()* special.length)];
+        console.log(randomSpecial);
+
+var generatePassword =getLowerCase.concat(getUpperCase, randomNum, randomSpecial)
+console.log(generatePassword)
+
+var passwordDisplayEl = document.querySelector("#password")
+
+passwordDisplayEl.textContent = (generatePassword)
 
 // Write password to the #password input
 function writePassword() {
@@ -50,22 +57,9 @@ function writePassword() {
 
   passwordText.value = password;
 
-// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)}
+generateBtn.addEventListener("click", writePassword)
+    var element = element.target;
 
-
-// Button is clicked to generate a password
-// User is prompted to input a length of 8-128 characters
-// User input is recorded
-// User is prompted for whether or not they'd like to include lowercase
-// User input is recorded
-// User is prompted for whether or not they like to inlcude uppercase
-// User input is recorded
-// User is prompted whether or not they'd like to include numeric
-// User input is recorded
-// User is prompted whether or not they'd like to include special charactors
-// User input is recorded
-// Computer generates at lease one of the selected criteria for a password
-// Computer displays the password in alert or on the page
+}
