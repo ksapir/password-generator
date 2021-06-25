74db85da2fecd8f0b84a775672b4fabf04e97ffe
Karen Pion <karenlpion@gmail.com>
2021-06-25 02:42:19 -0400

    structuring folders, adding comments in javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,6 @@
 // // Assignment Code
+
+// Global Variables
 var pwLength = [];
 var invalid = [isNaN(pwLength), pwLength>= 8, pwLength<= 128];
 var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K'", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"
@@ -6,10 +8,16 @@ var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K'", "L", "M"
 var lowerCase= "abcdefghijklmnopqurstuvwxyz".split("");
 var pwNum= [1,2,3,4,5,6,7,8,9,0];
 var special= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{","}", "|", ";",";", "'", "<", ">", "?", "/", ",", ".", "`", "~"];
+
+// 
 var generateBtn = document.querySelector("#generate");
 
-// function generatePassword(){
+// I was aware that I needed to creat a function like the following:
+//          function generatePassword(){
+// But unfortunately was unable to implement it.
 
+
+// When 'Generate Password' button is clicked, it prompts the following questions
 generateBtn.onclick = () => {
     var pwLength = prompt ("How long would like your password to be? (8-128)");
     console.log(pwLength);
@@ -23,6 +31,8 @@ generateBtn.onclick = () => {
             console.log(pwNum);
         var special = confirm ("Would you like to include special characters?");
             console.log(special);
+
+// If an invalid response is entered, user is asked to click 'Generate Password' again
 }  else if (invalid){
     alert("Invalid response. Click 'Generate Password Button' again");
     
@@ -31,6 +41,7 @@ generateBtn.onclick = () => {
 }
 }
 
+// Randomy generate an option from each variable array
    var getLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
         console.log(getLowerCase);
 
@@ -43,9 +54,12 @@ generateBtn.onclick = () => {
     var randomSpecial = special[Math.floor(Math.random()* special.length)];
         console.log(randomSpecial);
 
-var generatePassword =getLowerCase.concat(getUpperCase, randomNum, randomSpecial)
-console.log(generatePassword)
 
+// How I combined the randomly generated results from the arrays
+    var generatePassword =getLowerCase.concat(getUpperCase, randomNum, randomSpecial)
+        console.log(generatePassword)
+
+// Displaying the generated results, combined from each array in the password section
 var passwordDisplayEl = document.querySelector("#password")
 
 passwordDisplayEl.textContent = (generatePassword)
@@ -60,6 +74,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-    var element = element.target;
 
 }
